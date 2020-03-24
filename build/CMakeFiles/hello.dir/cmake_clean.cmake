644611cbd446c7ff53d5effc191182c8383c1ebf
yzymibook <yzyzero@gmail.com>
2020-03-24 18:09:03 +0800

    Initial commit

---
+++ build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/main.c.obj"
+  "hello.pdb"
+  "hello.exe"
+  "hello.exe.manifest"
+  "hello.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
